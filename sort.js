bc05ceb3fb1bebefe89094acd7e4c7d2fdb08f7d
Alice Pote <alice.writes.wrongs@gmail.com>
2020-05-25 10:09:30 -0400

    simple, very naive algorithm

---
+++ sort.js
@@ -0,0 +1,39 @@
+const fs = require("fs")
+const parse = require("csv-parse/lib/sync")
+
+const sentences = parse(fs.readFileSync("sentences.tsv"), {
+  delimiter: "\t"
+})
+
+const frequencyScores = {}
+
+sentences.forEach(([hindi]) => {
+  words = hindi.split(" ")
+
+  words.forEach(word  => {
+    if (frequencyScores[word]) {
+      frequencyScores[word]++
+    } else {
+      frequencyScores[word] = 1
+    }
+  })
+})
+
+const avg = xs => {
+  const sum = xs.reduce((acc, x) => acc + x, 0)
+  return sum / xs.length
+}
+
+const rankedSentences = sentences.map(([hindi, english]) => {
+  const words = hindi.split(" ")
+
+  const normalizedFrequencyScore = avg(words.map(word =>
+    frequencyScores[word]
+  ))
+
+  return [hindi, english, normalizedFrequencyScore]
+})
+
+rankedSentences.sort((a, b) => {
+  return a[2] + b[2]
+})
